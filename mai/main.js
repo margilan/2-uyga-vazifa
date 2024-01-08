7abd1b2dedd264c2b4d977dcc6f921619585854e
margilan <shermatovelbek88@gmail.com>
2024-01-08 13:56:06 +0500

    uyga vazifa tayyor

---
+++ mai/main.js
@@ -0,0 +1,9 @@
+const images = document.getElementById('img');
+const names = document.getElementById('name');
+const h1 = document.getElementById('desc');
+const button = document.querySelector('#btn')
+const description = document.getElementsByClassName('desc')
+
+ description && description.addEventListener('click' , function(){
+    alert("sz")
+ })
